4aaedf19e2b9f9a915296a33700252c987b1027b
Tatianna Torres <tat8@njit.edu>
2019-03-26 00:50:13 -0400

    Laravel setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+@section('content')
+
+
+
+
+
+
+   <h1> Hello World Welcome to my About Page </h1>
+
+   <big>Hello World Welcome to my About Page </big>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+
+   <h1>Hello World Welcome to my Homepage</h1>
+
+   <big> Hello World Welcome to my Home Page </big>
+@stop
